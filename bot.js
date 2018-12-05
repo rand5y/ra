7d2ecea3e462e83d9a6b8505530b1580a91e3a6a
ran1dyp <45611516+ran1dyp@users.noreply.github.com>
2018-12-05 13:40:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,49 @@ const moment = require('moment')
 const ms = require('ms')
 const prefix = '-';
 const jimp = require('jimp');
+client.on('message', message => {
+     var prefix = "-"
+if (message.content.startsWith(prefix + "uptime")) {
+    let uptime = client.uptime;
+ 
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+ 
+    while (notCompleted) {
+ 
+        if (uptime >= 8.64e+7) {
+ 
+            days++;
+            uptime -= 8.64e+7;
+ 
+        } else if (uptime >= 3.6e+6) {
+ 
+            hours++;
+            uptime -= 3.6e+6;
+ 
+        } else if (uptime >= 60000) {
+ 
+            minutes++;
+            uptime -= 60000;
+ 
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+ 
+        }
+ 
+        if (uptime < 1000)  notCompleted = false;
+ 
+    }
+ 
+    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+ 
+ 
+}
+});
 const welcome = JSON.parse(fs.readFileSync('./welcomer.json' , 'utf8'));
  
 client.on('message', message => {
