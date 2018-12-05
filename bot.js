bbc13383e708ca7e7f9ffb1dc6e7630d81ac24b0
ran1dyp <45611516+ran1dyp@users.noreply.github.com>
2018-12-05 02:38:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -222,27 +222,28 @@ client.on("message", message => {
                           }
 });
 
-	 client.on("message", message => {
-    
+	 client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
  
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('? | **ليس لديك الصلاحيه حتى تمسح الشات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-       
-        color: 0x06DF00,
-        description: "تم مسح الروم بنجآح„ ",
-       
-
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
-
-     
-}); 
+    if(command === "مسح","clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
 
 client.login(process.env.BOT_TOKEN);
