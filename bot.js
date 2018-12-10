aaa5a50f8d181e74082b31e3fd8b14f41cf97290
ran1dyp <45611516+ran1dyp@users.noreply.github.com>
2018-12-10 23:56:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -290,7 +290,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Randy ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Pànorama Server.. `,"http://twitch.tv/S-F")
+client.user.setGame(``,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 client.on('message', message => {
@@ -363,7 +363,10 @@ client.on('message', message => {
 -obc --> لنشر برودكاست لكل اعضاء السيرفر 
 -server --> معلومات السيرفر 
 رابط --> اظهار رابط السيرفر خآص 
-
+-setWelcomer --> تفعيل ترحيب بصوره 
+-toggleInvitedby --> تفعيل تم دعوته بواسطة 
+-toggleDmwelcomer --> تفعيل الترحيب علخاص 
+-toggleWelcomer -->توقيف الترحيب بالخاص و الصوره و تم دعوته 
 **
 
 
@@ -372,6 +375,46 @@ client.on('message', message => {
 `);
 
     }
+});
+const adminprefix = "";
+const developers = ['278515372020334592' ,'399353508429824000'];
+
+console.log("Hamooo");
+
+client.on('ready', () => {
+    console.log(`Logged as ${client.user.tag}`)
+})
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`تم تغيير البلاينق الى   ${argresult}`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`تَم تغيير الواتشينق الى   ${argresult}`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`تَم تغيير الليسينينق الى   ${argresult}`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/osama");
+      message.channel.send(`تم تغييرك حالتك بالتويتش الى   ${argresult}`)
+  }
+  if (message.content.startsWith(adminprefix + 'sn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`جاري تغيير الأسم لـ ..${argresult} `)
+} else
+if (message.content.startsWith(adminprefix + 'sa')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`جاري تغيير الأفتار... : `);
+}
 });
 
    client.on("message", message => {
